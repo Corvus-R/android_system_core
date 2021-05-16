bd3ab88c9022b89fe666250e8cfe3a40c806d718
StarkDroid <gamerprince.exp@gmail.com>
2021-05-16 15:17:36 +0200

    Revert "init: add vendor.*  keys to spoof safetynet"
    This reverts commit f5abe1ce8ed6f16ff48f34ed80b4e37dc214fa81.

--- init/property_service.cpp
+++ init/property_service.cpp
@@ -790,10 +790,6 @@ static const char *snet_prop_key[] = {
 	"ro.build.tags",
 	"ro.system.build.tags",
 	"ro.build.selinux",
-	"ro.vendor.boot.warranty_bit",
-	"ro.vendor.warranty_bit",
-	"vendor.boot.vbmeta.device_state",
-	"vendor.boot.verifiedbootstate",
 	NULL
 };
 
@@ -812,10 +808,6 @@ static const char *snet_prop_value[] = {
 	"release-keys",
 	"release-keys",
 	"0",
-	"0",
-	"0",
-	"locked",
-	"green",
 	NULL
 };
 
